2fdb60435d16a777f3c0fbbf0cd812ab790c183e
Emmet Halm <emmet.halm@gmail.com>
2024-01-25 15:39:02 -0500

    init flask to do app repo

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# /// = relative path, //// = absolute path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+
+@app.route("/")
+def home():
+    todo_list = Todo.query.all()
+    return render_template("base.html", todo_list=todo_list)
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
